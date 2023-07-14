2583a036236e4b4b376f858859bb51fed03c64f4
leosmaia21 <leosmaia21@gmail.com>
2023-07-14 18:51:57 +0100

    lexer deve estar completo

--- lexer.h
+++ lexer.h
@@ -6,7 +6,7 @@
 /*   By: ledos-sa <ledos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 16:46:58 by ledos-sa          #+#    #+#             */
-/*   Updated: 2023/07/14 18:30:17 by ledos-sa         ###   ########.fr       */
+/*   Updated: 2023/07/14 18:51:52 by ledos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ typedef enum s_token_type{
 	command,
 	flag,
 	redirect
-}	t_token_type;
+}	t_tokentype;
 
 typedef struct s_token{
 	char			*t;
-	t_token_type	type;
+	t_tokentype	type;
 	int				total;
 	int				index;
 }	t_token;
