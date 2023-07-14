7c27cda07a83fa9b4ebd7bf6428536fdfdffbf2b
leosmaia21 <leosmaia21@gmail.com>
2023-07-14 18:47:20 +0100

    lexer deve estar completo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ledos-sa <ledos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 15:53:46 by ledos-sa          #+#    #+#             */
-/*   Updated: 2023/07/14 18:42:01 by ledos-sa         ###   ########.fr       */
+/*   Updated: 2023/07/14 18:47:14 by ledos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,5 @@ int	main(void)
 {
 	char *str = readfromstdin();
 	t_token *tokens = dividetokens(str);
+
 }
