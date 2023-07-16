484f4c4bdc5294626c3e91c328bbcbc68d0053df
leosmaia21 <leosmaia21@gmail.com>
2023-07-16 14:55:49 +0100

    makefile martelado

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 SRCS = main.c lexer.c utils.c signals.c
 NAME = minishell
 CC = clang
-FLAGS = -Wall -g -Werror -Wextra -lreadline #-fsanitize=thread
+CFLAGS = -Wall -g -Wextra -lreadline 
 RM = rm -f
 OBJS= $(SRCS:.c=.o)
 INCS	= libft/libft.a
@@ -9,17 +9,17 @@ INCS	= libft/libft.a
 all: ${NAME}
 
 $(NAME):runlibft $(OBJS)
-		${CC} ${FLAGS} -o ${NAME} ${OBJS} ${INCS}
+		${CC} ${CFLAGS} -o ${NAME} ${OBJS} ${INCS}
 
 ignore:
-	${CC} ${FLAGS} -o ${NAME} ${OBJS} ${INCS}
+	${CC} ${CFLAGS} -o ${NAME} ${OBJS} ${INCS}
 
 clean:
 	${RM} ${OBJS}
 
 fclean:clean 
 	${RM} ${NAME}
-	make -C libft fclean
+	# make -C libft fclean
 
 re:fclean all
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bde-sous <bde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 15:53:46 by ledos-sa          #+#    #+#             */
-/*   Updated: 2023/07/15 18:21:23 by bde-sous         ###   ########.fr       */
+/*   Updated: 2023/07/16 14:28:22 by ledos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ int	main(int argc, char **argv, char **envp)
 
 	(void)argc;
 	(void)argv; 
+	(void)envp;
 	while(1)
 	{
 		signals();
