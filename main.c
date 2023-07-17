c44e805a4aceb50a5393ca42219fc8ddb569edff
leosmaia21 <leosmaia21@gmail.com>
2023-07-17 22:07:12 +0100

    maravilha maravilha

--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 /*   By: bde-sous <bde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 15:53:46 by ledos-sa          #+#    #+#             */
-/*   Updated: 2023/07/16 21:40:50 by ledos-sa         ###   ########.fr       */
+/*   Updated: 2023/07/17 15:52:19 by ledos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "minishell.h"
+#include <stdint.h>
 
 char *get_type(t_tokentype type)
 {
